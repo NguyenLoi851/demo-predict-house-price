3eaad63fa6ce68d502c25eeb0095406570aaf6e3
NguyenLoi851 <nguyensyloi851@gmail.com>
2023-02-01 21:49:14 +0700

    add api

--- temp.js
+++ temp.js
@@ -1,28 +1,28 @@
 import axios from "axios"
-import { useContext, useEffect, useState } from "react"
+import { useContext } from "react"
 import { HouseInfoContext } from "../context/houseInfo"
 
 export const HomePage = () => {
     const { houseInfo, setHouseInfo } = useContext(HouseInfoContext)
 
+    console.log(houseInfo)
+
     const handlePredict = async () => {
         const apiUrl = "http://127.0.0.1:8000/model"
-        const param = houseInfo
+        const param = {'Area':'120','Number_of_bedroom':'12','Type_of_house':'Nhà biệt thự','Useable_area':'120'}
         const result = await axios.post(apiUrl,param)
         console.log(result)
-        console.log(houseInfo)
     }
-    
     return (
         <div>
             <div>
-                Diện tích đất: <input placeholder="(m2)" onChange={e => setHouseInfo({ ...houseInfo, Area: e.target.value })}></input>
+                Diện tích đất: <input placeholder="(m2)" onChange={e => setHouseInfo({ ...houseInfo, area: e.target.value })}></input>
             </div>
             <div>
-                Diện tích sử dụng: <input placeholder="(m2)" onChange={e => setHouseInfo({ ...houseInfo, Useable_area: e.target.value })}></input>
+                Diện tích sử dụng: <input placeholder="(m2)" onChange={e => setHouseInfo({ ...houseInfo, useableArea: e.target.value })}></input>
             </div>
             <div>
-                Số phòng ngủ: <input type="number" placeholder="v.d. 3 (phòng)" onChange={e => setHouseInfo({ ...houseInfo, Number_of_bedroom: e.target.value })}></input>
+                Số phòng ngủ: <input type="number" placeholder="v.d. 3 (phòng)" onChange={e => setHouseInfo({ ...houseInfo, bedroomNumber: e.target.value })}></input>
             </div>
             <div>
                 Số phòng vệ sinh: <input type="number" placeholder="v.d. 4 (phòng)" onChange={e => setHouseInfo({ ...houseInfo, bathroomNumber: e.target.value })}></input>
@@ -33,37 +33,37 @@ export const HomePage = () => {
             <div>
                 <label htmlFor="legalDocuments">Giấy tờ pháp lý: </label>
                 <select name="legalDocuments" onChange={e => setHouseInfo({...houseInfo, legalDocuments: e.target.value})}>
-                    <option value="nan">nan</option>
-                    <option value="Đã có sổ'">Đã có sổ'</option>
-                    <option value="Đang chờ sổ'">Đang chờ sổ'</option>
-                    <option value="Giấy tờ khác">Giấy tờ khác</option>
+                    <option value="0">nan</option>
+                    <option value="1">Đã có sổ'</option>
+                    <option value="2">Đang chờ sổ'</option>
+                    <option value="3">Giấy tờ khác</option>
                 </select>
             </div>
             <div>
                 <label htmlFor="houseType">Loại hình nhà ở: </label>
-                <select name="houseType" onChange={e => setHouseInfo({...houseInfo, Type_of_house: e.target.value})}>
-                    <option value="nan">nan</option>
-                    <option value="Nhà ngõ">Nhà ngõ</option>
-                    <option value="Nhà mặt phố">Nhà mặt phố</option>
-                    <option value="Nhà biệt thự">Nhà biệt thự</option>
-                    <option value="Nhà phố liền kề">Nhà phố liền kề</option>
+                <select name="houseType" onChange={e => setHouseInfo({...houseInfo, houseType: e.target.value})}>
+                    <option value="0">nan</option>
+                    <option value="1">Nhà ngõ</option>
+                    <option value="2">Nhà mặt phố</option>
+                    <option value="3">Nhà biệt thự</option>
+                    <option value="4">Nhà phố liền kề</option>
                 </select>
             </div>
             <div>
                 <label htmlFor="furnitureStatus">Tình trạng nội thất: </label>
                 <select name="furnitureStatus" onChange={e => setHouseInfo({...houseInfo, furnitureStatus: e.target.value})}>
-                    <option value="nan">nan</option>
-                    <option value="Nội thất đầy đủ">Nội thất đầy đủ</option>
-                    <option value="Nội thất cao cấp">Nội thất cao cấp</option>
-                    <option value="Hoàn thiện cơ bả">Hoàn thiện cơ bản</option>
-                    <option value="Bàn giao thô">Bàn giao thô</option>
+                    <option value="0">nan</option>
+                    <option value="1">Nội thất đầy đủ</option>
+                    <option value="2">Nội thất cao cấp</option>
+                    <option value="3">Hoàn thiện cơ bản</option>
+                    <option value="4">Bàn giao thô</option>
                 </select>
             </div>
             <div>
                 <label htmlFor="location">Địa chỉ: </label>
                 <select name="location" onChange={e => setHouseInfo({...houseInfo, location: e.target.value})}>
-                    <option value="Hà Nội">Hà Nội</option>
-                    <option value="Hồ Chí Minh">Hồ Chí Minh</option>
+                    <option value="0">Hà Nội</option>
+                    <option value="1">Hồ Chí Minh</option>
                 </select>
             </div>
             <button onClick={handlePredict}>
